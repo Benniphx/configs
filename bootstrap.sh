d4647c4a6bdd96b99e5e70c949463347572a852f
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2015-02-22 12:35:37 +0200

    Tidied up bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# This script symlinks the configuration files to the home directory.
-#
+# This script symlinks the configs to the $HOME.
+# Run with -f to override the exising files at $HOME without confirmation.
+
 # On Windows (Vista and newer), Sublime Text configs can be symlinked by
 # opening a PowerShell as an administrator and running the following commands:
 #
@@ -9,82 +10,66 @@
 #   2. rmdir -recurse User
 #   3. cmd /c mklink /D User $env:userprofile\configs\sublime\User
 
-### CONFIGURATION ##############################################################
 
-# Subdirectory name for Linux/OS X configs
-POSIX_CONFIG_DIR_NAME='posix'
+SCRIPT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+OS_IDENTIFIER="${OSTYPE//[0-9.]/}"
 
-# Subdirectory name for the Sublime Text configs
-SUBLIME_CONFIG_DIR_NAME='sublime'
 
-# Sublime Text major version number (2 or 3)
-SUBLIME_VERSION=2
+### Configuration ##############################################################
 
-# Add files that should not be symlinked to the array below.
-DO_NOT_SYMLINK=(
-  'do_not_want.sh'
-)
+SUBLIME_VERSION=2       # 2 or 3
+CONFIG_PATH="$SCRIPT_PATH/posix"
+SUBLIME_CONFIG_PATH="$SCRIPT_PATH/sublime"
 
-### NO NEED TO EDIT BELOW ######################################################
+if [ "$OS_IDENTIFIER" == "darwin" ]; then
+    # File names inside the CONFIG_PATH are enough
+    DO_NOT_SYMLINK=(
+      'fonts.conf'
+    )
 
-OS_IDENTIFIER=${OSTYPE//[0-9.]/}
+    sublime_user_path="$HOME/Library/Application Support/Sublime Text $SUBLIME_VERSION/Packages/User"
+else
+    sublime_user_path="$HOME/.config/sublime-text-$SUBLIME_VERSION/Packages/User"
+fi
 
-THIS_DIR=`dirname "${BASH_SOURCE}"`
-CONFIG_DIR=`cd $THIS_DIR/$POSIX_CONFIG_DIR_NAME && pwd`
-SUBLIME_CONFIG_DIR=`cd $THIS_DIR/$SUBLIME_CONFIG_DIR_NAME && pwd`
 
-skip_or_create_symlink() {
-    symlink_path="$2/$1"
-    if [[ -h "$symlink_path" ]] || [[ -e "$symlink_path" ]] && [[ $force != 1 ]]; then
-        echo "   Skipping: $1 (already exists, use -f to override)"
-        return
-    fi
-    if [[ -h "$symlink_path" ]] || [[ -e "$symlink_path" ]] && [[ $force == 1 ]]; then
-        rm -rf "$symlink_path"
-    fi
-    echo "   Symlinking: $3/$1 -> $symlink_path"
-    ln -s "$3/$1" "$2"
-}
+### Helpers ####################################################################
 
-in_array() {
+function in_array() {
     for e in "${@:2}"; do
         [[ "$e" = "$1" ]] && break;
     done;
 }
 
+
+### Main #######################################################################
+
 while getopts 'f' OPTION; do
     case "$OPTION" in
         f)
-            force=1
-        ;;
+            LN_OPTS='f'
+            ;;
         *)
             exit 1
-        ;;
+            ;;
     esac
 done
 
-# Sublime Text configs
-echo "-> Initializing Sublime Text $SUBLIME_VERSION configs and packages"
-if [[ "$OS_IDENTIFIER" == 'darwin' ]]; then
-    destination="$HOME/Library/Application Support/Sublime Text $SUBLIME_VERSION/Packages"
-else
-    destination="$HOME/.config/sublime-text-$SUBLIME_VERSION/Packages"
-fi
-skip_or_create_symlink 'User' "$destination" "$SUBLIME_CONFIG_DIR"
-
-# POSIX configs
-echo '-> Updating config files'
-for source_file in `ls -A $CONFIG_DIR`; do
-    if ( in_array "$source_file" "${DO_NOT_SYMLINK[@]}" ); then
-        echo "   Ignoring: $source_file"
+# Configs
+for source_file_name in `ls -A $CONFIG_PATH`; do
+    if ( in_array "$source_file_name" "${DO_NOT_SYMLINK[@]}" ); then
+        echo "   Ignoring: $source_file_name"
     else
-        skip_or_create_symlink "$source_file" "$HOME" "$CONFIG_DIR"
+        ln -snvi$LN_OPTS "$CONFIG_PATH/$source_file_name" "$HOME/$source_file_name"
     fi
 done
 
-pushd $PWD > /dev/null
+# Sublime User packages and configuration
+ln -snvi$LN_OPTS "$SUBLIME_CONFIG_PATH" "$sublime_user_path"
 
-echo '-> Clone vim bundles'
+# Vim bundles as git submodules
+pushd "$CONFIG_PATH" > /dev/null
 git submodule --quiet update --init --recursive
-
 popd > /dev/null
+
+exit 0
