e808db86e9195c579b60d61ece7aebd0583fa0e2
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2015-03-23 19:46:35 +0200

    Fixed checking if the file is to be ignored

--- symlink.sh
+++ symlink.sh
@@ -34,9 +34,11 @@ fi
 ### Helpers ####################################################################
 
 function in_array() {
-    for e in "${@:2}"; do
-        [[ "$e" = "$1" ]] && break;
-    done;
+    local e
+    for e in "${@:2}";
+        do [[ "$e" == "$1" ]] && return 0; 
+    done
+    return 1
 }
 
 
