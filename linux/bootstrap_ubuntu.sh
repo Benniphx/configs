68073ce26133678797fa3aa1f1b57ab4d430249f
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2015-05-19 23:37:58 +0300

    Added screen, tmux. Do a system upgrade first. Minor splitting.

--- linux/bootstrap_ubuntu.sh
+++ linux/bootstrap_ubuntu.sh
@@ -6,10 +6,15 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Add Oracle Java repository
+sudo apt-add-repository ppa:webupd8team/java
 
+# Do a full upgrade first to get up to date
+sudo apt-get update
+sudo apt-get dist-upgrade
 
 # Utilities
-sudo apt-get install build-essential htop xsel git vim curl wget colordiff sysv-rc-conf
+sudo apt-get install build-essential htop xsel git vim curl wget colordiff screen tmux sysv-rc-conf synaptic
 
 # Browsers
 sudo apt-get install chromium-browser firefox
@@ -17,11 +22,18 @@ sudo apt-get install chromium-browser firefox
 # Compile-time requirements
 sudo apt-get install openssl libssl-dev libreadline-dev libsqlite3-dev libbz2-dev zlib1g-dev
 
-# Fonts (MS + Terminus)
-sudo apt-get install ttf-mscorefonts-installer xfonts-terminus xfonts-terminus-oblique xfonts-terminus-dos
+# Fonts: Terminus
+sudo apt-get install xfonts-terminus xfonts-terminus-oblique xfonts-terminus-dos
+
+# Fonts: Microsoft true type core fonts
+sudo apt-get install ttf-mscorefonts-installer
+
+# Install Oracle Java JDK
+sudo apt-get install oracle-java8-installer
 
 # XFCE goodies
 sudo apt-get install xfce4-goodies gtk2-engines-murrine gtk2-engines-pixbuf
 
 # Restricted extras (Xubuntu only)
 sudo apt-get install xubuntu-restricted-extras
+
