a5b9d1d6f3e2847610db578c83b2ceab4c2d10be
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2015-11-29 02:36:10 +0200

    General cleanup

--- apps/xubuntu_apt.sh
+++ apps/xubuntu_apt.sh
@@ -7,7 +7,7 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Add Oracle Java repository
-sudo apt-add-repository ppa:webupd8team/java
+sudo apt-add-repository -y ppa:webupd8team/java
 
 # Do a full upgrade first to get up to date
 sudo apt-get update
@@ -15,16 +15,16 @@ sudo apt-get dist-upgrade
 
 # Utilities
 sudo apt-get install -y build-essential htop xsel git vim curl wget colordiff \
-    screen tmux synaptic
+    screen tmux synaptic openssl
 
 # Firefox
 sudo apt-get install -y firefox
 
-# Compile-time requirements
-sudo apt-get install -y openssl libssl-dev libreadline-dev libsqlite3-dev \
+# Usual compile-time requirements
+sudo apt-get install -y libssl-dev libreadline-dev libsqlite3-dev \
     libbz2-dev zlib1g-dev
 
-# Fonts: Microsoft true type core fonts
+# Microsoft true type core fonts
 sudo apt-get install -y ttf-mscorefonts-installer
 
 # Install Oracle Java JDK
