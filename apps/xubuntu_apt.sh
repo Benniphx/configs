3cc267c8516005e67191159548ff1d4ed2fef7ae
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2015-11-30 19:53:43 +0200

    Add vim-gtk (for +xterm_clipboard) and chromium-browser

--- apps/xubuntu_apt.sh
+++ apps/xubuntu_apt.sh
@@ -1,44 +1,44 @@
 #!/usr/bin/env bash
 
-# Ask for the administrator password upfront.
+# Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
+# Keep-alive: update existing `sudo` time stamp until the script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Add Oracle Java repository
 sudo apt-add-repository -y ppa:webupd8team/java
 
-# Do a full upgrade first to get up to date
+# Do a full upgrade first to get the system up to date
 sudo apt-get update
 sudo apt-get dist-upgrade
 
-# Utilities
-sudo apt-get install -y build-essential htop xsel git vim curl wget colordiff \
-    screen tmux synaptic openssl
+# utilities
+sudo apt-get install -y build-essential htop xsel git vim vim-gtk curl wget \
+    colordiff screen tmux synaptic openssl
 
-# Firefox
-sudo apt-get install -y firefox
+# browsers
+sudo apt-get install -y chromium-browser firefox
 
-# Usual compile-time requirements
+# usual compile-time requirements
 sudo apt-get install -y libssl-dev libreadline-dev libsqlite3-dev \
     libbz2-dev zlib1g-dev
 
 # Microsoft true type core fonts
 sudo apt-get install -y ttf-mscorefonts-installer
 
-# Install Oracle Java JDK
+# Oracle Java JDK
 sudo apt-get install -y oracle-java8-installer
 
 # XFCE goodies
 sudo apt-get install -y xfce4-goodies gtk2-engines-murrine gtk2-engines-pixbuf
 
-# Xmonad related
+# xmonad related
 sudo apt-get install -y xmonad xmobar dmenu gmrun trayer yeganesh scrot \
     suckless-tools
 
-# Rbenv and ruby-build
+# rbenv and ruby-build
 sudo apt-get install -y rbenv ruby-build
 
-# Restricted extras (Xubuntu only)
+# restricted extras
 sudo apt-get install -y xubuntu-restricted-extras
