b04ab63b31a5772c34fec8faed5e6759f943346d
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2016-02-26 23:07:59 +0200

    Add --with-default-names

--- apps/osx_brew.sh
+++ apps/osx_brew.sh
@@ -18,14 +18,14 @@ brew install coreutils
 brew tap homebrew/dupes
 brew install binutils
 brew install diffutils
-brew install ed --default-names
+brew install ed --with-default-names
 brew install findutils --with-default-names
 brew install gawk
 brew install gnu-indent --with-default-names
 brew install gnu-sed --with-default-names
 brew install gnu-tar --with-default-names
 brew install gnu-which --with-default-names
-brew install gnutls
+brew install gnutls --with-default-names
 brew install grep --with-default-names
 brew install gzip
 brew install screen
