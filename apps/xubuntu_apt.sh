b6004c88ed6cb68fe7c1dbaacbe74373b924b5ed
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2016-09-06 00:18:45 +0300

    Improve comments

--- apps/xubuntu_apt.sh
+++ apps/xubuntu_apt.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Ask for the administrator password upfront
+# Ask for the sudo password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until the script has finished
+# Keep-alive: update existing `sudo` timestamp until the script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Add Oracle Java repository
@@ -41,7 +41,7 @@ sudo apt install -y firefox
 # Google Chrome
 sudo apt install -y google-chrome-stable
 
-# Profanity
+# Profanity (Irssi style XMPP client)
 sudo apt install -y profanity
 
 # Microsoft true type core fonts
