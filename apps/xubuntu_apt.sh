af99c53df04dde6f2ff148a767057f78daac1494
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2016-09-06 00:55:48 +0300

    Prevent adding duplicate Google Chrome repository

--- apps/xubuntu_apt.sh
+++ apps/xubuntu_apt.sh
@@ -14,7 +14,7 @@ sudo add-apt-repository -y ppa:neovim-ppa/unstable
 
 # Add Google Chrome repository
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 
 # Do a full upgrade first to get the system up to date
 sudo apt update
