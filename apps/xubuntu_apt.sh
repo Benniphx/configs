1c7cf5d1a4129ed90467ac8d5ffeea257731d14b
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2016-09-25 00:22:58 +0300

    Fix line wrap

--- apps/xubuntu_apt.sh
+++ apps/xubuntu_apt.sh
@@ -26,7 +26,8 @@ sudo apt install -y build-essential htop openssl python-setuptools synaptic \
   vim vim-gtk wget xsel
 
 # Google Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | \
+  sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 sudo apt update
 sudo apt install -y google-chrome-stable
