484a18124ef3c3963c5633118e0900afb90f2710
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2016-10-21 20:16:44 +0300

    Move common to xbrew.sh so they can be installed using Linuxbrew as well

--- apps/osx_brew.sh
+++ apps/osx_brew.sh
@@ -1,23 +1,11 @@
 #!/usr/bin/env bash
 
-# Based on:
-# https://www.topbug.net/blog/2013/04/14/install-and-use-gnu-command-line-tools-in-mac-os-x/
-
 # Install Homebrew if not exists
 if ! which brew >/dev/null; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew update
-brew upgrade
-
-# Might be already installed but homebrew/dupes usually has newer versions
-brew tap homebrew/dupes
-brew install diffutils
-brew install gpatch
-brew install gzip
-brew install less
-brew install nano
 
 # GNU utils are g-prefixed (but can also be installed --with-default-names)
 brew install coreutils
@@ -30,18 +18,10 @@ brew install gnu-which
 brew install grep
 brew install make
 
-# Commonly used from homebrew-core
-brew install bash
+# Mac OS X specific
 brew install htop-osx
-brew install p7zip
-brew install pstree
 brew install macvim --with-override-system-vim
 brew install reattach-to-user-namespace
-brew install tree
-brew install watch
-brew install wdiff --with-gettext
-brew install wget
-brew install zsh
 
 # Set up Homebrew Cask
 brew install caskroom/cask/brew-cask
--- apps/xbrew.sh
+++ apps/xbrew.sh
@@ -1,35 +1,49 @@
 #!/usr/bin/env bash
 
 # These can be installed by Homebrew (OS X) or Linuxbrew (Linux distros)
-
 brew update
-brew upgrade
 
-# Commonly used
+# From homebrew-dupes
+brew tap homebrew/dupes
+brew install diffutils
+brew install gpatch
+brew install gzip
+brew install file-formula
+brew install less
+brew install nano
+brew install rsync
+brew install screen
+
+# From homebrew-core
 brew install ack
 brew install autojump
+brew install bash
 brew install bash-completion
-brew install ctags
 brew install colordiff
+brew install ctags
 brew install curl
 brew install diff-so-fancy
-brew install file-formula
 brew install git
 brew install neovim/neovim/neovim
+brew install p7zip
+brew install pstree
 brew install ranger
-brew install rsync
 brew install shellcheck
-brew install screen
 brew install scmpuff
 brew install thefuck
 brew install the_silver_searcher
 brew install tig
 brew install tmux
+brew install tree
+brew install watch
+brew install wdiff --with-gettext
+brew install wget
+brew install zsh
 
 # Version managers
 brew install node nvm
 brew install pyenv pyenv-virtualenv
 brew install rbenv ruby-build
 
-# Do final clean ups
+# Do final clean up
 brew cleanup
