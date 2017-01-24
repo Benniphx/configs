8f137140714e00a3d80fcffba7e0f04ed9c31f6f
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2017-01-24 05:11:52 +0200

    Combine brew installs ts `install_apps.sh`

--- install_apps.sh
+++ install_apps.sh
@@ -57,4 +57,59 @@ brew install awscli
 brew install aws-shell
 brew install aws-elasticbeanstalk
 
+# OS X only brews
+if [[ "$OSTYPE" == darwin* ]]; then
+  # GNU utils are g-prefixed (but can also be installed --with-default-names)
+  brew install coreutils
+  brew install findutils
+  brew install gawk
+  brew install gnu-indent
+  brew install gnu-sed
+  brew install gnu-tar
+  brew install gnu-which
+  brew install homebrew/dupes/grep
+  brew install homebrew/dupes/make
+
+  # These are on Linuxbrew's tap but currently compile on OS X only
+  brew install shellcheck
+  brew install watch
+
+  # OS X fix for tmux+pbcopy+pbpaste combination
+  brew install reattach-to-user-namespace
+
+  # Set up Homebrew Cask
+  brew tap caskroom/cask
+  brew tap caskroom/versions
+  brew tap buo/cask-upgrade
+
+  # Terminals
+  brew cask install iterm2
+
+  # Editors
+  brew cask install sublime-text
+
+  # Browsers
+  brew cask install google-chrome
+
+  # Sync utilities
+  brew cask install google-drive
+
+  # Players
+  brew cask install spotify
+
+  # Window managers
+  brew cask install amethyst
+
+  # Keyboard remapping tools
+  brew cask install ukelele
+
+  # Password managers
+  brew cask install keepassx
+
+  # GPG Suite
+  brew cask install gpgtools
+
+  brew cask cleanup
+fi
+
 brew cleanup
