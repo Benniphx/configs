f33d1ab2b0750d2c353d30d3e154870ada246057
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2017-01-24 05:03:11 +0200

    Comment out rarely used brews

--- apps/xbrew.sh
+++ apps/xbrew.sh
@@ -11,10 +11,10 @@ fi
 brew update
 
 # homebrew-dupes
-brew install homebrew/dupes/diffutils
-brew install homebrew/dupes/gpatch
+#brew install homebrew/dupes/diffutils
+#brew install homebrew/dupes/gpatch
 brew install homebrew/dupes/gzip
-brew install homebrew/dupes/file-formula
+#brew install homebrew/dupes/file-formula
 brew install homebrew/dupes/less
 brew install homebrew/dupes/nano
 brew install homebrew/dupes/rsync
