d63fc122795b1106a9a4351626b13156e99e1fb4
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2017-03-12 16:26:32 +0200

    Comment out SoundCleod (OS X only) - too buggy

--- install_apps.sh
+++ install_apps.sh
@@ -94,7 +94,7 @@ if [[ "$OSTYPE" == darwin* ]]; then
   brew cask install macdown
   # brew cask install psequel
   # brew cask install sequel-pro
-  brew cask install soundcleod
+  # brew cask install soundcleod
   brew cask install spotify
   brew cask install sublime-text
   brew cask install the-unarchiver
