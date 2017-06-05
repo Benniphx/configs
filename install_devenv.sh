2a394dc8103ac7e4f7ca21611e1e7b542fa60f2a
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2017-06-05 23:11:06 +0300

    Prefer `pip` for AWS tools over `brew`

--- install_devenv.sh
+++ install_devenv.sh
@@ -10,21 +10,73 @@ fi
 
 brew update
 
-# Version managers
+### Pyenv ######################################################################
+
 brew install pyenv pyenv-virtualenv
+if [[ -d "$HOME/.pyenv" ]]; then
+  pyenv_path="$HOME/.pyenv/bin"
+  export PATH="$pyenv_path:$PATH"
+  export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+  eval "$(pyenv init -)"
+  if which pyenv-virtualenv-init > /dev/null; then
+    eval "$(pyenv virtualenv-init -)"
+  fi
+fi
+
+# Python 2.7
+python2_version="2.7.13"
+pyenv install --skip-existing "$python2_version"
+pyenv shell "$python2_version"
+pip install --upgrade pip virtualenv
+pip install awscli
+pip install awscfncli
+pip install awsebcli
+pip install awslogs
+pip install aws-shell
+pip install saws
+#pip install s3cmd
+
+# Python 3.6
+python3_version="3.6.1"
+pyenv install --skip-existing "$python3_version"
+pyenv shell "$python3_version"
+pip install --upgrade pip virtualenv
+pip install awscli
+pip install awscfncli
+pip install awsebcli
+pip install awslogs
+pip install aws-shell
+pip install saws
+#pip install s3cmd
+
+### Rbenv ######################################################################
+
 brew install rbenv ruby-build
+if [[ -d "$HOME/.rbenv" ]]; then
+  rbenv_path="$HOME/.rbenv/bin"
+  export PATH="$rbenv_path:$PATH"
+  eval "$(rbenv init -)"
+fi
+
+# Ruby 2.4
+ruby_version="3.6.1"
+rbenv install --skip-existing "$ruby_version"
+rbenv shell "$ruby_version"
+gem install bundler
+
+### nvm ########################################################################
+
 brew install nvm
+export NVM_DIR="$HOME/.nvm"
+[[ ! -d "$NVM_DIR" ]] && mkdir -p "$NVM_DIR"
+. "$(brew --prefix nvm)/nvm.sh"
 
-# Install Pythons from brew though
-brew install python
-brew install python3
+# Node LTS
+nvm install v7.7.1
 
-# AWS CLI tools
-brew install awscli
-brew install aws-shell
-brew install aws-elasticbeanstalk
+### utils ######################################################################
 
-# Utilities
+brew install s3cmd
 brew install terraform
 brew install git-secrets
 
